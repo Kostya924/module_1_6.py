6a620368f75539ca7b437086354df3ba50b4b13d
Kostya924 <podsekaev68@mail.ru>
2024-09-02 22:51:27 +0300

    Add files via upload

---
+++ modul_1_6..py
@@ -0,0 +1,19 @@
+my_dict={"Kostya":1968, "Tatyana":1971, "Julia":2000}
+print(my_dict)
+print(my_dict['Kostya'])
+my_dict ["Anton"] = 1975
+print(my_dict)
+my_dict["Olga"] = 1980
+my_dict ["Galina"] = 1985
+print(my_dict)
+del my_dict ["Galina"]
+print(my_dict)
+my_set = {1,2,3,4,4,3,2,5,"Denis","Olga","Olga",True}
+print (my_set)
+print(my_set.add(10))
+print(my_set)
+print(my_set.add("igor"))
+print(my_set.remove("Denis"))
+print(my_set)
+
+
