6c03d71f2936fcacb05da5c55b6df9672a38dbe1
Kostya924 <podsekaev68@mail.ru>
2024-09-08 11:54:10 +0300

    Add files via upload

--- modul_1_6..py
+++ modul_1_6..py
@@ -6,8 +6,8 @@ print(my_dict)
 my_dict["Olga"] = 1980
 my_dict ["Galina"] = 1985
 print(my_dict)
-del my_dict ["Galina"]
-print(my_dict)
+a = my_dict.pop ("Galina")
+print(a)
 my_set = {1,2,3,4,4,3,2,5,"Denis","Olga","Olga",True}
 print (my_set)
 print(my_set.add(10))
@@ -15,5 +15,6 @@ print(my_set)
 print(my_set.add("igor"))
 print(my_set.remove("Denis"))
 print(my_set)
+
 
 
